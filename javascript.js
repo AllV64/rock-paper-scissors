bc43c2a46052fdba07eb56a1a649d2565ef06c22
AllV64 <114334244+AllV64@users.noreply.github.com>
2023-08-29 14:46:59 +0200

    Turn pseudo code into code

--- javascript.js
+++ javascript.js
@@ -16,11 +16,24 @@ function playerSelection(){
 }
 
 //declare function playGame with 2 parameters, containing the variables playerSelected and computerSelected
-//if the playerSelected is not rock, paper or scissors
-//return string "You have entered invalid text, you entered {playerSelected"}
+function playGame(playerSelected, computerSelected){
+    return (playerSelected !== "rock" 
+         || playerSelected !== "paper" 
+         || playerSelected !== "scissors"
+           ) ? `You have entered invalid text. You entred ${playerSelected}.`
+         : (playerSelected == "rock" && computerSelected == "scissors" 
+         || playerSelected == "paper" && computerSelected == "rock"
+         || playerSelected == "scissors" && computerSelected == "paper"
+           ) ? `You won! ${playerSelected} beats ${computerSelected}` 
+         : (computerSelected == "rock" && playerSelected == "scissors" 
+         || computerSelected == "paper" && playerSelected == "rock"
+         || computerSelected == "scissors" && playerSelected == "paper"
+           ) ? `You lost! ${computerSelected} beats ${playerSelected}` 
+         : `It's a tie! ${computerSelected} can't beat ${playerSelected}`;
 //else if the playerSelected is an option that wins over what computerSelected is
 //return string "You won! {playerSelected} beats {computerSelected}"
 //else if the computerSelected is an option that wins over what playerSelected is
 //return string "You lost! {computerSelected} beats {userSelected}"
 //else if the computerSelected and playerSelected is the same
 //return string "It's a tie! {computerSelected} can't beat {playerSelected}"
+}
