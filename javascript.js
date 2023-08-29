25a017ce4fff9d0f1d0cf8acad0cafdddf8b8a3d
AllV64 <114334244+AllV64@users.noreply.github.com>
2023-08-29 10:54:46 +0200

    turn pseudocode into code

--- javascript.js
+++ javascript.js
@@ -1,10 +1,13 @@
-//declare function called computerSelection
-//create a variable that's named computerChoice and is an empty string
-//create a variable that's named randomNumber and takes in a random integer between 1 and 3
-//IF randomNumber = 1
-//  computerChoice equals "rock"
-//ELSE IF randomNumber = 2
-//  computerChoice equals "paper"
-//ELSE
-//  computerChoice equals "scissors"
-//return the value of computerChoice
+function computerSelection(){
+    let computerChoice = "";
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+    if (randomNumber == 1){
+        computerChoice == "rock";
+    } else if (randomNumber == 2){
+        computerChoice == "paper";
+    } else {
+        computerChoice == "scissors";
+    }
+    return computerChoice;
+}
