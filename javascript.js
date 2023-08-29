6a9c0606a3c1963e52657f19c0b6f06f0c575ce0
AllV64 <114334244+AllV64@users.noreply.github.com>
2023-08-29 13:54:44 +0200

    Add pseudo code for the game being played section

--- javascript.js
+++ javascript.js
@@ -14,3 +14,13 @@ function playerSelection(){
     let playerChoice = prompt("Enter either 'rock', 'paper' or 'scissors' here") 
     return playerChoice.toLocaleLowerCase();
 }
+
+//declare function playGame with 2 parameters, containing the variables playerSelected and computerSelected
+//if the playerSelected is not rock, paper or scissors
+//return string "You have entered invalid text, you entered {playerSelected"}
+//else if the playerSelected is an option that wins over what computerSelected is
+//return string "You won! {playerSelected} beats {computerSelected}"
+//else if the computerSelected is an option that wins over what playerSelected is
+//return string "You lost! {computerSelected} beats {userSelected}"
+//else if the computerSelected and playerSelected is the same
+//return string "It's a tie! {computerSelected} can't beat {playerSelected}"
