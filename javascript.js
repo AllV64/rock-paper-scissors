420dfe4fc3397a7b57094d070da2db31155c60a4
AllV64 <114334244+AllV64@users.noreply.github.com>
2023-09-06 08:58:35 +0200

    Chane conditional statment for PlayRound

--- javascript.js
+++ javascript.js
@@ -27,19 +27,15 @@ playerSelect.addEventListener('click', () => {
 });
 
 function playRound(playerSelected, computerSelected){
-    return (playerSelected !== "rock" 
-         && playerSelected !== "paper" 
-         && playerSelected !== "scissors"
-           ) ? `You have entered invalid text. You entred ${playerSelected}.`
-         : (playerSelected == "rock" && computerSelected == "scissors" 
-         || playerSelected == "paper" && computerSelected == "rock"
-         || playerSelected == "scissors" && computerSelected == "paper"
-           ) ? `You won! ${playerSelected} beats ${computerSelected}` 
-         : (computerSelected == "rock" && playerSelected == "scissors" 
-         || computerSelected == "paper" && playerSelected == "rock"
-         || computerSelected == "scissors" && playerSelected == "paper"
-           ) ? `You lost! ${computerSelected} beats ${playerSelected}` 
-         : `It's a tie! ${computerSelected} can't beat ${playerSelected}`;
+    if (playerSelected == computerSelected){
+      return `It's a tie! ${computerSelected} can't beat ${playerSelected}`;
+    } else if (playerSelected == "rock" && computerSelected == "scissors" 
+            || playerSelected == "paper" && computerSelected == "rock"
+            || playerSelected == "scissors" && computerSelected == "paper"){
+      return `You won! ${playerSelected} beats ${computerSelected}`;
+    } else {
+      return `You lost! ${computerSelected} beats ${playerSelected}` 
+    }
 }
 
 let roundMessage = "";
