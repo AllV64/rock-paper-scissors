6576aa60eab265250390da34b351938d2da0da67
AllV64 <114334244+AllV64@users.noreply.github.com>
2023-09-06 14:14:07 +0200

    Add call to PlayRound

--- javascript.js
+++ javascript.js
@@ -14,6 +14,7 @@ const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
+    playRound(playerSelection(), computerSelection())
   });
 });
 
